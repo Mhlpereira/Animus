c14fbd706cf164f257b4b9ee843a730e97059655
Mário Henrique <mariohenriquelp@gmail.com>
2025-02-04 12:08:50 -0300

    Fix modelagem de dados

--- db.sql
+++ db.sql
@@ -1,62 +1,50 @@
 CREATE TABLE "users" (
-	"id" UUID NOT NULL UNIQUE,
-	"email" VARCHAR(255) NOT NULL UNIQUE,
-	"password" VARCHAR(255) NOT NULL,
-	"created_at" DATE NOT NULL,
-	PRIMARY KEY("id")
+  "id" UUID PRIMARY KEY DEFAULT,
+  "email" VARCHAR(255) NOT NULL UNIQUE,
+  "password" VARCHAR(255) NOT NULL,
+  "created_at" TIMESTAMP NOT NULL
 );
-
 
 CREATE TABLE "customers" (
-	"id" UUID NOT NULL UNIQUE,
-	"name" VARCHAR(255) NOT NULL,
-	"birthday" DATE NOT NULL,
-	"created_at" DATE NOT NULL,
-	"updated_at" DATE,
-	PRIMARY KEY("id")
+  "id" UUID PRIMARY KEY DEFAULT,
+  "name" VARCHAR(255) NOT NULL,
+  "birthday" DATE NOT NULL,
+  "created_at" TIMESTAMP NOT NULL,
+  "updated_at" TIMESTAMP,
+  "user_id" UUID UNIQUE,
+  "group_id" UUID,
+  FOREIGN KEY("user_id") REFERENCES "users"("id") ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY("group_id") REFERENCES "groups"("id") ON UPDATE CASCADE ON DELETE SET NULL
 );
-
 
 CREATE TABLE "groups" (
-	"id" UUID NOT NULL UNIQUE,
-	"name" VARCHAR(255) NOT NULL,
-	"owner_id" UUID NOT NULL,
-	"created_at" DATE NOT NULL,
-	"description" VARCHAR(255),
-	"updated_at" DATE,
-	PRIMARY KEY("id")
+  "id" UUID PRIMARY KEY DEFAULT,
+  "name" VARCHAR(255) NOT NULL,
+  "owner_id" UUID NOT NULL,
+  "created_at" TIMESTAMP NOT NULL,
+  "description" VARCHAR(255),
+  "updated_at" TIMESTAMP,
+  FOREIGN KEY("owner_id") REFERENCES "users"("id") ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+CREATE TABLE "tipos_grupo" (
+  "id" UUID PRIMARY KEY DEFAULT,
+  "name" VARCHAR(255) NOT NULL UNIQUE
+);
 
 CREATE TABLE "turmas" (
-	"id" UUID NOT NULL UNIQUE,
-	"name" VARCHAR(255) NOT NULL,
-	"starts" DATE NOT NULL,
-	"max_capacity" INTEGER,
-	"created_at" DATE NOT NULL,
-	"end" DATE NOT NULL,
-	PRIMARY KEY("id")
+  "id" UUID PRIMARY KEY DEFAULT,
+  "name" VARCHAR(255) NOT NULL,
+  "starts" DATE NOT NULL,
+  "max_capacity" INTEGER,
+  "created_at" TIMESTAMP NOT NULL,
+  "end" DATE NOT NULL
 );
-
 
 CREATE TABLE "turma_aluno" (
-	"id" UUID NOT NULL UNIQUE,
-	PRIMARY KEY("id")
+  "id" UUID PRIMARY KEY DEFAULT,
+  "turma_id" UUID NOT NULL,
+  "customer_id" UUID NOT NULL,
+  FOREIGN KEY("turma_id") REFERENCES "turmas"("id") ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY("customer_id") REFERENCES "customers"("id") ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-
-ALTER TABLE "customers"
-ADD FOREIGN KEY("id") REFERENCES "groups"("id")
-ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE "users"
-ADD FOREIGN KEY("id") REFERENCES "customers"("id")
-ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE "customers"
-ADD FOREIGN KEY("id") REFERENCES "turma_aluno"("id")
-ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE "turmas"
-ADD FOREIGN KEY("id") REFERENCES "turma_aluno"("id")
-ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE "groups"
-ADD FOREIGN KEY("id") REFERENCES "turmas"("id")
-ON UPDATE NO ACTION ON DELETE NO ACTION;
