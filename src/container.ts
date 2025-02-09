1b653b83b3b0f4de0909b416a6aec4b6c3291057
Mário Henrique <mariohenriquelp@gmail.com>
2025-02-08 23:56:39 -0300

    colocando dto de output

--- src/container.ts
+++ src/container.ts
@@ -4,11 +4,15 @@ import { UserModel } from './user/user-model';
 import { UserService } from './user/user-service';
 import { CustomerModel } from './customer/customer-model';
 import { AuthMiddleware } from './middleware/auth-middleware';
+import { ICustomerModel, ICustomerService } from './customer/customer-interface';
+import { CustomerService } from './customer/customer-service';
 
 const container = new Container();
 
 container.bind<IUserModel>('IUserModel').to(UserModel);
 container.bind<IUserService>('IUserService').to(UserService);
+container.bind<ICustomerModel>('ICustomerModel').to(CustomerModel);
+container.bind<ICustomerService>('ICustomerService').to(CustomerService);
 container.bind<AuthMiddleware>(AuthMiddleware).toSelf();
 
 export { container };
--- src/customer/customer-service.ts
+++ src/customer/customer-service.ts
@@ -1,16 +1,24 @@
-import { CustomerModel } from "./customer-model";
+import { inject, injectable } from "inversify";
 import { CustomerCreateDTO } from "./DTO/create-customer-DTO";
+import { ICustomerModel } from "./customer-interface";
+import { requestBody } from "inversify-express-utils";
+import { stringToBytes } from "uuid/dist/cjs/v35";
 
-
+@injectable()
 export class CustomerService{
 
 
-    constructor(){}
+    constructor(@inject('ICustomerModel') private customerModel: ICustomerModel){}
 
 
-    async createCustomer(data: CustomerCreateDTO){
+    async createCustomer(@requestBody() data: CustomerCreateDTO){
 
-        const {customer} = await CustomerModel.createCustomer(CustomerCreateDTO)
+        const {customer} = await this.customerModel.createCustomer({
+            name: data.name,
+            birthday: data.birthday,
+            userId: data.userId,
+        });
 
+        return customer;
     }
 }
